6da4917f75ae58bbfdc1c6e22332c67a29fbe716
ALhorm <75273270+ALhorm@users.noreply.github.com>
2021-05-20 17:40:40 +0300

    Update buppy.py

--- buppy.py
+++ buppy.py
@@ -1,8 +1,8 @@
 class Backup:
-	def save(filename, extension):
+	def save(filename):
 		filedata = open(filename + ".py", "r")
-		file = open("backup_" + filename + "." + extension, "w")
+		file = open("backup_" + filename + ".py", "w")
 		file.write(filedata.read())
-		print("\nThe file " + filename + ".py" + " is backed up and saved to the file backup_" + filename + "." + extension)
+		print("\nThe file " + filename + ".py" + " is backed up and saved to the file backup_" + filename + ".py")
 		file.close()
 		filedata.close()
